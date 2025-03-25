88dedacf3ea954f0a58b6874366816c5e99ff902
AlexanderNdiukwu <ndiukwuchukwuemeka@gmail.com>
2025-03-25 01:11:17 +0100

    'add'

--- frontend/src/TodoBody/Todobody.jsx
+++ frontend/src/TodoBody/Todobody.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
+import Todoheader from '../Todoheader'
+import Todos from './Todos'
 
 export default function Todobody() {
   return (
     <div className='lg:mx-8 mx-4'>
 
       <div className='bg-slate-200 w-full'>
-        
+        <Todoheader/>
+        <Todos/>
+
+
+
+
         
 
       </div>
